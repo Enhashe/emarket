630795dc4dbac81e785e59e827e4f082bdd8fa1f
Enhashe <pendalf.gray@gmail.com>
2016-11-10 13:26:49 +0700

    Category view is ready

--- src/app/marketApp/view/pages/category.php
+++ src/app/marketApp/view/pages/category.php
@@ -7,13 +7,24 @@
 						$id = $products[$key]['id'];
 						$cat_id =  $products[$key]['cat_id'];
 						$title = $products[$key]['title'];
+						$image = !empty($products[$key]['image']) ? $products[$key]['image'] : '/images/icons/bag.png' ;
 						$description = $products[$key]['description'];
 						$price = $products[$key]['price'];
 						$output .= <<<EOD
-							<a href="/catalogue/$cat_id/$id">	
-								<div class="catalog-item">
-								<h3>$title</h3>
+						<a href="/catalogue/$cat_id/$id">	
+						<div class="catalog-item">
+							<h3>$title</h3>
+							<img src="$image"><br>
+							$description
+							<div class="price">
+								<div class="price-value">
+									Price: $price
 								</div>
+								<div class="price-img">
+								<img src="/images/icons/coin.png">
+								</div>
+							</div>
+						</div>
 							</a>
 EOD;
 					}
